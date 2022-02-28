caca9e77199b4a1b275b54fd0cc460303650ed72
BerkeToksun <100554267+BerkeToksun@users.noreply.github.com>
2022-02-28 11:07:30 +0300

    Kodlarım

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sasa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+        ArrayList sehirler = new ArrayList();
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+
+
+            for (int i = 0;i < sehirler.Count; i++)
+            {
+                listBox1.Items.Add(sehirler[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(textBox1.Text);
+        }
+       
+        
+    }
+}
